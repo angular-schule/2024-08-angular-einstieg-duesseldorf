b5faa7bff868a077ce2e4630332acf8e28ab0fd4
Ferdinand Malcher <ferdinand@malcher.media>
2024-08-26 10:53:18 +0200

    Input und Property Binding

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'app-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.scss'
+})
+export class BookComponent {
+
+  // Input: hier dürfen Daten von der Elternkomponente
+  // hineinfließen. von oben nach unten
+  @Input() book?: Book;
+
+}
