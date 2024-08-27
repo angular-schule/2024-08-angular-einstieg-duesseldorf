e9b988fb56f7e92cf9ccf21acb7b26264bc7b74e
Ferdinand Malcher <ferdinand@malcher.media>
2024-08-27 12:37:40 +0200

    Detailseite: HTTP mit toSignal()

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book-details',
@@ -18,12 +19,13 @@ export class BookDetailsComponent {
   private bs = inject(BookStoreService);
 
   book = signal<Book | undefined>(undefined);
+  bookX = toSignal(this.bs.getSingle(this.route.snapshot.paramMap.get('isbn')!))
 
   constructor() {
     const isbn = this.route.snapshot.paramMap.get('isbn')!; // Non-Null Assertion
     this.bs.getSingle(isbn).subscribe(book => {
       this.book.set(book);
-    })
+    });
   }
 
 }
