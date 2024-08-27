2ebb8523d3379315332d50602abb684801b88edc
Ferdinand Malcher <ferdinand@malcher.media>
2024-08-27 11:10:45 +0200

    HTTP

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -18,32 +18,10 @@ export class DashboardComponent {
   private bs = inject(BookStoreService);
 
   constructor() {
-    this.books.set([
-      {
-        isbn: '12345',
-        title: 'Angular',
-        description: 'Grundlagen und Best Practices',
-        rating: 5,
-        price: 36.9,
-        firstThumbnailUrl: 'https://api.angular.schule/avatar/5',
-      },
-      {
-        isbn: '23456',
-        title: 'React',
-        description: 'Das Framework von Facebook',
-        rating: 3,
-        price: 32.9,
-        firstThumbnailUrl: 'https://api.angular.schule/avatar/3',
-      },
-      {
-        isbn: '34567',
-        title: 'Vue.js',
-        description: 'Grün und vunderbar',
-        rating: 5,
-        price: 29.9,
-        firstThumbnailUrl: 'https://api.angular.schule/avatar/4',
-      },
-    ]);
+    this.bs.getAll().subscribe(books => {
+      // this.books = books;
+      this.books.set(books);
+    })
   }
 
 
--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Book } from './book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookStoreService {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://api.angular.schule';
 
-  foo = Math.random();
+  getAll() {
+    return this.http.get<Book[]>(`${this.apiUrl}/books`);
+    // this.http.get(this.apiUrl + '/books');
+  }
 
-  constructor() { }
+  getSingle(isbn: string) {
+    return this.http.get<Book>(`${this.apiUrl}/books/${isbn}`);
+  }
 }
